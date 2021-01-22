8d76dbf7794e3a52f3e2c955baccdf86299247f6
Synthetica <i.synthetica@gmail.com>
2021-01-22 12:35:53 +0200

    Test console errors 01

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log('\x1b[32m%s\x1b[0m','Hostnames to add:\n');
 
 console.error('\x1b[31m%s\x1b[0m','Cloufront API [create hostname] Error');
+
+throw `Missing Environment Variables [CLOUDFLARE_API_TOKEN]`;
